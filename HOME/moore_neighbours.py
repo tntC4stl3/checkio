1ad56658557b6c1b3156beadd0f6becfcb9dfc82
Jonathan Guo <jonathang@synnex.com>
2015-04-07 20:51:59 +0800

    new solutions

---
+++ HOME/moore_neighbours.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# coding: utf-8
+
+def count_neighbours(grid, row, col):
+    surround = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1), (0, 1),
+        (1, -1), (1, 0), (1, 1)
+    ]
+
+    surround = [(item[0]+row, item[1]+col) for item in surround]
+    len_row = len(grid)
+    len_col = len(grid[0])
+
+    count = 0
+    for (row_1, col_1) in surround:
+        if row_1 < 0 or row_1 >= len_row or \
+            col_1 < 0 or col_1 >= len_col:
+            pass
+        else:
+            count += grid[row_1][col_1]
+    return count
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_neighbours(
+        ((1, 0, 0, 1, 0),
+         (0, 1, 0, 0, 0),
+         (0, 0, 1, 0, 1),
+         (1, 0, 0, 0, 0),
+         (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
+
