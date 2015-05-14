a31f5ac73fb3d7bc29cb477a6d0d5b746a28aeac
Jonathan Guo <jonathang@synnex.com>
2015-05-14 12:46:27 +0800

    three words

---
+++ ELEMENTARY/thress_words.py
@@ -0,0 +1,20 @@
+def checkio(words):
+    count = 0
+    ret = True
+    for word in words.split(' '):
+        if word.isdigit():
+            count = 0
+        else:
+            count += 1
+        if count == 3:
+            return True
+    else:
+        return False
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(u"Hello World hello") == True, "Hello"
+    assert checkio(u"He is 123 man") == False, "123 man"
+    assert checkio(u"1 2 3 4") == False, "Digits"
+    assert checkio(u"bla bla bla bla") == True, "Bla Bla"
+    assert checkio(u"Hi") == False, "Hi"
