e9b86f9dd3280685eb7e9a30ef814036b2724863
Jonathan Guo <jonathang@synnex.com>
2015-05-14 16:16:44 +0800

    digits multiplication

---
+++ ELEMENTARY/digits_multiplication.py
@@ -0,0 +1,14 @@
+def checkio(number):
+    nums = map(int, list(str(number)))
+    result = 1
+    for num in nums:
+        if num > 0:
+            result *= num
+    return result
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(123405) == 120
+    assert checkio(999) == 729
+    assert checkio(1000) == 1
+    assert checkio(1111) == 1
