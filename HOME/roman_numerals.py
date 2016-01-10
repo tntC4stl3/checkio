bca611ad000a52a105b1a3409463f812fcd92254
Jonathan Guo <blackhumour.gj@gmail.com>
2016-01-10 22:02:57 +0800

    Roman Numerals

---
+++ HOME/roman_numerals.py
@@ -0,0 +1,29 @@
+def checkio(data):
+    number = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I'
+    }
+    romans = []
+    for num in sorted(number.keys(), reverse=True):
+        count = data / num
+        romans.append(number[num] * count)
+        data %= num
+    return "".join(romans)
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
